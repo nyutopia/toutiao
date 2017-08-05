86db35b19ee10790fa7513b04e342bcfed5b5544
nyutopia <791206628@qq.com>
2017-08-05 19:03:39 +0800

    资讯发布 图片上传

---
+++ src/main/java/com/ny/toutiao/controller/MessageController.java
@@ -0,0 +1,21 @@
+package com.ny.toutiao.controller;
+
+import com.ny.toutiao.service.NewsService;
+import com.ny.toutiao.util.ToutiaoUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * Created by ny on 2017/8/5.
+ */
+@Controller
+public class MessageController {
+
+}
--- src/main/java/com/ny/toutiao/dao/NewsDAO.java
+++ src/main/java/com/ny/toutiao/dao/NewsDAO.java
@@ -22,4 +22,10 @@ public interface NewsDAO {
     List<News> selectByUserIdAndOffset(@Param("userId") int userId,@Param("offset") int offset,
                                        @Param("limit") int limit);
 
+    @Update({"update ", TABLE_NAME, " set comment_count = #{commentCount} where id=#{id}"})
+    int updateCommentCount(@Param("id") int id, @Param("commentCount") int commentCount);
+
+    @Select({"select ", SELECT_FIELDS , " from ", TABLE_NAME, " where id=#{id}"})
+    News getById(int id);
+
 }
